7c96e3d114f55c75c7d844cadb3ad9b15018c7a6
John Doe <SlySpellar@gmail.com>
2022-10-30 16:41:38 +0200

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+function sumOfAreas(squareSide, rectSideA, rectSideB, circleRadius, ellipseMajorAxis, ellipseMinorAxis) {
+	// write your code here
+}
+
+module.exports = sumOfAreas;
---
+++ system/system.js
@@ -0,0 +1,3 @@
+const sumOfAreas = require('../index');
+
+sumOfAreas(20, 30, 40, 10, 15, 10);
