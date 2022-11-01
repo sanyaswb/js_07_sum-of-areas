cec237bc9858c83daa07a02678ead64b335b014e
sanyaswb <sanyaswb@gmail.com>
2022-11-01 05:04:04 +0200

    completed 07_sum-of-areas

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumOfAreas(squareSide, rectSideA, rectSideB, circleRadius, ellipseMajorAxis, ellipseMinorAxis) {
-	// write your code here
+	const areaSquare = squareSide ** 2;
+	const areaRectangle = rectSideA * rectSideB;
+	const areaCircle = Math.PI * (circleRadius ** 2);
+	const areaAxis = Math.PI * ellipseMajorAxis * ellipseMinorAxis;
+	const totalAreaOfFigures = areaSquare + areaRectangle + areaCircle + areaAxis;
+
+	return totalAreaOfFigures;
 }
 
 module.exports = sumOfAreas;
